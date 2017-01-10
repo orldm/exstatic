1d5528ecc7229e6c72f5ef5b26801d01ec19292c
Dmitri <orldm@mail.ru>
2017-01-10 13:53:26 +0200

    add styles and server message

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, function() {
+    console.log('server is running on port 3000');
+});
